d37d51a4be65e29313bd686c6de7727cd9c3a898
Dulneth210229 <dulneth.sa@gmail.com>
2024-12-15 21:25:12 +0530

    Select all the elements

---
+++ script.js
@@ -0,0 +1,9 @@
+//! Select the elements
+const calculateBtn = document.getElementById("calculateBtn");
+const amountInput = document.getElementById("amount");
+const interestInput = document.getElementById("interest");
+const yearsInput = document.getElementById("years");
+//Summary
+const monthelyPayment = document.getElementById("montly");
+const totalPayment = document.getElementById("total");
+const totalInterest = document.getElementById("totalInterest");
